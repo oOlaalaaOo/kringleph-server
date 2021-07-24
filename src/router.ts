3c3e5cd8643726c49de55fae95646f238ddab39d
Benjamin Espanola <espanola.benjamin@gmail.com>
2021-07-25 01:20:18 +0800

    added default response for testing

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,13 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import authRouter from "./modules/auth/auth.router";
 import membershipRouter from "./modules/membership/membership.router";
 import userRouter from "./modules/user/user.router";
 
 const router = express.Router();
 
+router.use("/", (req: Request, res: Response, next: NextFunction) => {
+  res.send("Hello World!");
+});
 router.use("/auth", authRouter);
 router.use("/membership", membershipRouter);
 router.use("/user", userRouter);
